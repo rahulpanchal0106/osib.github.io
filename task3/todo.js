e442f25ba4df424f786ac595401e1d33c5cf929a
rm5901960@gmail.com <rm5901960@gmail.com>
2023-05-28 23:45:24 +0530

    update

--- task3/todo.js
+++ task3/todo.js
@@ -1,77 +1,43 @@
-//Defining variables
-var button = document.getElementById("button");
-var tasks = document.getElementById("tasks");
 var list = document.getElementById("list");
 var sound = "sounds/pop.mp3";
-//Addtask function
-function addtask(){
-    
-    let input = document.getElementById("input").value;
-    if(input===""){
-        window.alert("Enter a task!");
-    }else{
-        var li = document.createElement('li');
-        let del = document.createElement('button');
-        console.log(li);
-        li.innerHTML=input;
-        del.onclick=()=>{
-            playpop();
-        }
-        console.log(input);
-        list.appendChild(li);   
-        li.appendChild(del);
-        document.getElementById("input").value="";
+
+window.addEventListener("load", function() {
+  var tasks = localStorage.getItem("tasks");
+  if (tasks) {
+    list.innerHTML = tasks;
+  }
+});
+
+function addtask() {
+  let input = document.getElementById("input").value;
+  if (input === "") {
+    window.alert("Enter a task!");
+  } else {
+    var li = document.createElement('li');
+    let del = document.createElement('button');
+    li.innerHTML = input;
+    del.onclick = () => {
+      playpop();
     }
-    
-    
+    list.appendChild(li);
+    li.appendChild(del);
+    document.getElementById("input").value = "";
+
+    localStorage.setItem("tasks", list.innerHTML);
+  }
 }
-function playpop(){
-    var audio = new Audio(sound);
-    audio.play(); 
+
+function playpop() {
+  var audio = new Audio(sound);
+  audio.play();
 }
 
-list.addEventListener("click",function(e){
-    if(e.target.tagName==="LI"){
-        e.target.classList.toggle("checked");
-    }else if(e.target.tagName==="BUTTON"){
-        e.target.parentElement.remove();
-    }
-})
-
-
-
-//add a cartoon animation after a task added
-
-
-// Retrieve tasks from local storage
-function getTasksFromLocalStorage() {
-    const tasksString = localStorage.getItem('list');
-    if (tasksString) {
-      return JSON.parse(tasksString);
-    } else {
-      return [];
-    }
+list.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+    
+    localStorage.setItem("tasks", list.innerHTML);
   }
-  
-  // Save tasks to local storage
-  function saveTasksToLocalStorage(list) {
-    localStorage.setItem('list', JSON.stringify(list));
-  }
-  
-  // Add a task
-  function addTask(input) {
-    const list = getTasksFromLocalStorage();
-    tasks.push(input);
-    saveTasksToLocalStorage(list);
-  }
-  
-//   // Example usage
-//   const taskInput = document.getElementById('task-input');
-//   const addTaskButton = document.getElementById('add-task-button');
-  
-//   addTaskButton.addEventListener('click', function() {
-//     const task = taskInput.value;
-//     addTask(task);
-//     taskInput.value = '';
-//   });
-  
+});
