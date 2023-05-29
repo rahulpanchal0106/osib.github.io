99e523cb42090bb9030252f3d1e01c0e4c2a2dcf
Rahul Panchal <rm5901960@gmail.com>
2023-05-29 10:41:39 +0530

    Updating the UI/UI

--- task3/todo.js
+++ task3/todo.js
@@ -1,12 +1,29 @@
 var list = document.getElementById("list");
 var sound = "sounds/pop.mp3";
+var tasksDiv = document.getElementById("tasks");
 
 window.addEventListener("load", function() {
   var tasks = localStorage.getItem("tasks");
   if (tasks) {
     list.innerHTML = tasks;
   }
+  changeH1();
 });
+
+function changeH1(){
+  let taskL = list.childElementCount;
+  var note = document.getElementById("notice");
+  if(taskL<=0){
+    console.log("Task len: "+ taskL);
+    document.getElementById("h1").innerHTML="No task to do";
+    note.style.display="flex";
+    note.innerHTML = "No tasks are pending";
+  }else if(taskL>0){
+    console.log("Task len: "+ taskL);
+    document.getElementById("h1").innerHTML="Seizing the Day with Purpose";
+    note.style.display="none";
+  }
+}
 
 function addtask() {
   let input = document.getElementById("input").value;
@@ -15,20 +32,20 @@ function addtask() {
   } else {
     var li = document.createElement('li');
     let del = document.createElement('button');
+    del.setAttribute("id","delete");
     li.innerHTML = input;
-    del.onclick = () => {
-      playpop();
-    }
+    
     list.appendChild(li);
     li.appendChild(del);
     document.getElementById("input").value = "";
-
+    changeH1();
     localStorage.setItem("tasks", list.innerHTML);
   }
 }
 
 function playpop() {
   var audio = new Audio(sound);
+  audio.volume="1";
   audio.play();
 }
 
@@ -37,7 +54,9 @@ list.addEventListener("click", function(e) {
     e.target.classList.toggle("checked");
   } else if (e.target.tagName === "BUTTON") {
     e.target.parentElement.remove();
-    
-    localStorage.setItem("tasks", list.innerHTML);
+    changeH1();
+    playpop();
   }
+  
+  localStorage.setItem("tasks", list.innerHTML);
 });
